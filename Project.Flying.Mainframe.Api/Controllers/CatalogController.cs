9ae455736207c56a1d066ead30229e3115561db3
Izabela <izabelakurtis4@gmail.com>
2025-03-26 13:16:07 -0400

    Modified the Catalog Controller with 2 Items Objects (Pg.10)

--- Project.Flying.Mainframe.Api/Controllers/CatalogController.cs
+++ Project.Flying.Mainframe.Api/Controllers/CatalogController.cs
@@ -4,10 +4,16 @@ using Project.Flying.Mainframe.Domain.Catalog;
 namespace Project.Flying.Mainframe.Api.Controllers {
     [ApiController]
     [Route("[controller]")]
-    public class CatalogController : ControllerBase {
+    public class CatalogController : ControllerBase 
+    {
         [HttpGet]
         public IActionResult GetItems() {
-            return Ok("Hello World.");
+            var items = new List<Item>() {
+                new Item("Shirt", "Ohio State shirt.", "Nike", 29.99m),
+                new Item("Shorts", "Ohio State shorts.", "Nike", 44.99m)
+            }; 
+
+            return Ok(items);
         }
 
     }
--- Project.Flying.Mainframe.Api/Program.cs
+++ Project.Flying.Mainframe.Api/Program.cs
@@ -1,7 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi 
+builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
@@ -12,7 +13,8 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-app.UseHttpsRedirection();
+app.UseHttpsRedirection(); 
+app.MapControllers(); 
 
 var summaries = new[]
 {
