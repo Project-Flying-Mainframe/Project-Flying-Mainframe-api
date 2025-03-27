d89ba8d49cb70deed0a5994929267365bc398cea
Quincy Li <quincylee200108@gmail.com>
2025-03-27 07:38:51 -0400

    Generated OrdersController and integrated orders API

--- Project.Flying.Mainframe.Api/Controllers/CatalogController.cs
+++ Project.Flying.Mainframe.Api/Controllers/CatalogController.cs
@@ -1,53 +1,87 @@
-using Microsoft.AspNetCore.Mvc; 
-using Project.Flying.Mainframe.Domain.Catalog; 
-using Project.Flying.Mainframe.Data; 
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Project.Flying.Mainframe.Domain.Catalog;
+using Project.Flying.Mainframe.Data;
+using System.Linq;
 
-namespace Project.Flying.Mainframe.Api.Controllers {
+namespace Project.Flying.Mainframe.Api.Controllers
+{
     [ApiController]
     [Route("/catalog")]
-    public class CatalogController : ControllerBase 
+    public class CatalogController : ControllerBase
     {
-        private readonly StoreContext _db; 
+        private readonly StoreContext _db;
 
         public CatalogController(StoreContext db)
-        { 
-            _db = db; 
+        {
+            _db = db;
         }
+
         [HttpGet]
-        public IActionResult GetItems() {
+        public IActionResult GetItems()
+        {
             return Ok(_db.Items);
-        } 
+        }
 
-        [HttpGet("{id:int}")] 
-        public IActionResult GetItem(int id) {
-            var item = new Item("Shirt", "Ohio State shirt.", "Nike", 29.99m);
-            item.Id = id; 
-
+        [HttpGet("{id:int}")]
+        public IActionResult GetItem(int id)
+        {
+            var item = _db.Items.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
             return Ok(item);
-        } 
+        }
 
         [HttpPost]
-        public IActionResult Post(Item item){
-            return Created("/catalog/42", item);
+        public IActionResult Post(Item item)
+        {
+            _db.Items.Add(item);
+            _db.SaveChanges();
+            return CreatedAtAction(nameof(GetItem), new { id = item.Id }, item);
         }
-        [HttpPost("{id:int}/ratings")] 
-        public IActionResult PostRating(int id, [FromBody] Rating rating){
-            var item = new Item("Shirt", "Ohio State shirt.", "Nike", 29.99m);
-            item.Id = id; 
-            item.AddRating(rating); 
 
+        [HttpPost("{id:int}/ratings")]
+        public IActionResult PostRating(int id, [FromBody] Rating rating)
+        {
+            var item = _db.Items.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            item.AddRating(rating);
+            _db.SaveChanges();
             return Ok(item);
-        } 
+        }
 
         [HttpPut("{id:int}")]
-        public IActionResult Put(int id, Item item){
-            return NoContent(); 
-        }  
-
-        [HttpDelete("{id:int}")]
-        public IActionResult Delete(int id, Item item){
-            return NoContent(); 
+        public IActionResult PutItem(int id, [FromBody] Item item)
+        {
+            if (id != item.Id)
+            {
+                return BadRequest();
+            }
+            if (_db.Items.Find(id) == null)
+            {
+                return NotFound();
+            }
+            _db.Entry(item).State = EntityState.Modified;
+            _db.SaveChanges();
+            return NoContent();
         }
 
+        [HttpDelete("{id:int}")]
+        public IActionResult DeleteItem(int id)
+        {
+            var item = _db.Items.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            _db.Items.Remove(item);
+            _db.SaveChanges();
+            return Ok();
+        }
     }
 }
--- Project.Flying.Mainframe.Api/Program.cs
+++ Project.Flying.Mainframe.Api/Program.cs
@@ -1,50 +1,24 @@
-using Project.Flying.Mainframe.Data;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Sqlite;
+using Project.Flying.Mainframe.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllers();
-builder.Services.AddDbContext<StoreContext>(options => options.UseSqlite("Data Source=.../Registrar.sqlite", 
-    b => b.MigrationsAssembly("Project.Flying.Mainframe.Api"))
-    );
-
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi 
- 
+builder.Services.AddDbContext<StoreContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("StoreContext") ?? "Data Source=C:\\Users\\25729\\Project-Flying-Mainframe-api\\Project.Flying.Mainframe.Api\\store.db", b => b.MigrationsAssembly("Project.Flying.Mainframe.Data")));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection(); 
-app.MapControllers(); 
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.UseHttpsRedirection();
+app.MapControllers();
 
 app.Run();
 
