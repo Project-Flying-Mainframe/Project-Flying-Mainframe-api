fa5cb16341de1651bb898f09dd262f2a3143f66b
Izabela <izabelakurtis4@gmail.com>
2025-04-05 18:37:26 -0400

    Part 1 Section 2

---
+++ tests/Project.Flying.Mainframe.Domain.tests/RatingTests.cs
@@ -0,0 +1,30 @@
+﻿using Project.Flying.Mainframe.Domain;
+using Project.Flying.Mainframe.Domain.Catalog;
+namespace Project.Flying.Mainframe.Domain.Tests;
+
+[TestClass]
+public class RatingTests{
+
+
+    [TestMethod]
+    public void Can_Create_New_Rating(){
+        //arrange 
+        var rating = new Rating(1, "Mike", "Great fit!");
+        
+        //act (empty)
+
+        //assert 
+
+        Assert.AreEqual(1, rating.Stars); 
+        Assert.AreEqual("Mike", rating.UserName); 
+        Assert.AreEqual("Great fit!", rating.Review);
+
+    }
+
+    [TestMethod] 
+    [ExpectedException(typeof(ArgumentException))] 
+    public void Cannot_Create_Rating_With_Invalid_Stars() {
+        //arrange
+        var rating = new Rating(0, "Mike", "Great fit!"); 
+    }
+}
